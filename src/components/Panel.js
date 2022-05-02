4425e5ee99457f9627a59bb512f9ed464ac40f59
John Davis <johnmdavis125@gmail.com>
2022-05-02 05:19:11 -0400

    implement pagination in Panel component

--- src/components/Panel.js
+++ src/components/Panel.js
@@ -1,10 +1,41 @@
 import React from 'react';
+import { useState } from 'react'; 
 import ImageCard from './ImageCard';
 import "./Panel.css";
 
 const Panel = ({images, apiName}) => {
     
-    const renderedImages = images.map((image) => {       
+    const [currentPage, setCurrentPage] = useState(0);
+    
+    const incrementResults = () => {
+        setCurrentPage(currentPage + 1);
+    }    
+    const decrementResults = () => {
+        setCurrentPage(currentPage - 1);
+    }
+    
+    const numImages = images.length; 
+    const resultsPerPage = 10; 
+    const numPages = numImages / resultsPerPage; 
+    let filteredArray = []; 
+    const filterImageArrayToCurrentPanelPage = (arrImages) => {
+        const firstImageOnPage = resultsPerPage * currentPage;
+        const lastImageOnPage = firstImageOnPage + resultsPerPage;
+        filteredArray = arrImages.slice(firstImageOnPage, lastImageOnPage); 
+        return filteredArray; 
+    }
+ 
+    if (numImages){
+        filterImageArrayToCurrentPanelPage(images); 
+    }
+
+    let rightButtonDisabled = false;
+    if (currentPage === numPages - 1){rightButtonDisabled = true;}
+    let leftButtonDisabled = false; 
+    if (currentPage === 0){leftButtonDisabled = true;}
+
+    
+    const renderedImages = filteredArray.map((image) => {       
         return (
             <ImageCard key={image.id} image={image} />
         )
@@ -13,7 +44,9 @@ const Panel = ({images, apiName}) => {
     return (
         <div>
             <div>{apiName} Panel</div>
+            <button onClick={decrementResults} disabled={leftButtonDisabled}>Left Arrow</button>
             <div className="imagesContainer">{renderedImages}</div>        
+            <button onClick={incrementResults} disabled={rightButtonDisabled}>Right Arrow</button>
         </div>
     )
 }
--- src/components/Search.js
+++ src/components/Search.js
@@ -11,7 +11,10 @@ const Search = () => {
     async function searchUnsplash(searchTerm) {
         try {
           const response = await axios.get('https://api.unsplash.com/search/photos', {
-            params: { query: searchTerm },  
+            params: { 
+                query: searchTerm,
+                per_page: 30
+            },  
             headers: {
                   Authorization: `Client-ID ${UNSPLASH_KEY}`
               }
