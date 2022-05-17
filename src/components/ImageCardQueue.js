45fec5ead025171b2fc13484f4128fb4a873b66a
John Davis <johnmdavis125@gmail.com>
2022-05-17 19:49:58 -0400

    refactor ImageCardQueue to utilize config object instead of overly verbose conditional return statement

--- src/components/ImageCardQueue.js
+++ src/components/ImageCardQueue.js
@@ -1,124 +1,95 @@
-import React from 'react';
+import React from 'react'; 
 import { useState, useEffect, useRef } from 'react';
 import Card from 'react-bootstrap/Card';
 import CloseButton from 'react-bootstrap/CloseButton';
 import './componentStyles/ImageCardQueue.css';
 
 const ImageCardQueue = ({ image, deselectFromQueue }) => {
-
-        const deselect = () => {
-            deselectFromQueue(image); 
-        }
-
-       // Image Positioning
-       const [spans, setSpans] = useState(0);
-       const ref = useRef(); 
-       
-       useEffect(() => {
-           if (ref.current){
-           const current = ref.current;
-           current.addEventListener('load', () => {
-           const height = ref.current.clientHeight;
-           const numSpans = Math.ceil(height / 10); 
-           setSpans(numSpans);
-           })
-        } 
-       },[]);
-    let imageTitle;
-    if (image.hasOwnProperty('urls')){
-        console.log(image); 
-        imageTitle = image.description ? image.description : 'Untitled';         
-        return (
-            <Card style={{ gridRowEnd: `span ${spans}`, width: '800px'}} className='queueCard'>
-                <div style={{display: 'flex'}}>
-                <Card.Img 
-                    className="queueImage" 
-                    ref={ref}
-                    src={image.urls.regular} 
-                    variant="top" 
-                />
-                <Card.Body>
-                    <div style={{display: 'flex', justifyContent: 'space-between'}}>
-                        <Card.Title>{imageTitle}</Card.Title>
-                        <CloseButton 
-                            onClick={deselect}
-                            className="closeButton"
-                            style={{color: 'white', opacity: '0.5'}}
-                            variant="dark">
-                        </CloseButton>
-                    </div>
-                    <Card.Text>
-                        <p>Image Source: Unsplash</p>
-                        <p>Photographer: {image.user.name}</p>
-                        <a href={image.user.links.portfolio}>Photographer Portfolio</a>
-                    </Card.Text>
-                </Card.Body>
-                </div>
-            </Card>
-        )
-    } else if (image.hasOwnProperty('src')){
-        console.log(image);
-        imageTitle = image.alt ? image.alt : 'Untitled';
-        return (
-            <Card style={{ gridRowEnd: `span ${spans}`, width: '800px'}} className='queueCard'>
-                <div style={{display: 'flex'}}>
-                <Card.Img 
-                    className="queueImage" 
-                    ref={ref}
-                    src={image.src.medium} 
-                    variant="top" 
-                />
-                <Card.Body>
-                    <div style={{display: 'flex', justifyContent: 'space-between'}}>
-                        <Card.Title>{imageTitle}</Card.Title>
-                        <CloseButton 
-                            onClick={deselect}
-                            className="closeButton"
-                            style={{color: 'white', opacity: '0.5'}}
-                            variant="dark">
-                        </CloseButton>
-                    </div> 
-                    <Card.Text>
-                        <p>Image Source: Pexels</p>
-                        <p>Photographer: {image.photographer}</p>
-                        <a href={image.photographer_url}>Photographer Portfolio</a> 
-                    </Card.Text>
-                </Card.Body>
-                </div>
-            </Card>
-        ) 
-    } else if (image.hasOwnProperty('webformatURL')){
-        console.log(image);
-        imageTitle = image.tags ? image.tags : 'Untitled';
-        return (
-            <Card style={{ gridRowEnd: `span ${spans}`, width: '800px'}} className='queueCard'>
-                <div style={{display: 'flex'}}>
-                <Card.Img 
-                    className="queueImage" 
-                    ref={ref}
-                    src={image.webformatURL} 
-                    variant="top" 
-                />
-                <Card.Body>
-                    <div style={{display: 'flex', justifyContent: 'space-between'}}>
-                        <Card.Title>{imageTitle}</Card.Title>
-                        <CloseButton 
-                            onClick={deselect}
-                            className="closeButton"
-                            style={{color: 'white', opacity: '0.5'}}
-                            variant="dark">
-                        </CloseButton>
-                    </div>
-                    <Card.Text>
-                        <p>Image Source: Pexels</p>
-                        <p>Photographer: {image.user}</p>
-                        <a href={image.userImageURL}>Photographer Portfolio</a>
-                    </Card.Text>
-                </Card.Body>
-                </div>
-            </Card>
-        )
+    
+    const deselect = () => {
+        deselectFromQueue(image); 
     }
+
+   // Image Positioning
+   const [spans, setSpans] = useState(0);
+   const ref = useRef(); 
+   
+   useEffect(() => {
+       if (ref.current){
+       const current = ref.current;
+       current.addEventListener('load', () => {
+       const height = ref.current.clientHeight;
+       const numSpans = Math.ceil(height / 10); 
+       setSpans(numSpans);
+       })
+    } 
+   },[]);
+
+let config = {
+    imageTitle: '',
+    imgSrc: '',
+    apiName: '',
+    photographer: '',
+    portfolioLink: ''
+  };
+
+  if (image.hasOwnProperty('urls')){
+    config = {
+        imageTitle: image.description ? image.description : 'Untitled',
+        imgSrc: image.urls.regular,
+        apiName: 'Unsplash',
+        photographer: image.user.name,
+        portfolioLink: image.user.links.portfolio
+    }
+  } else if (image.hasOwnProperty('src')){
+    config = {
+        imageTitle: image.alt ? image.alt : 'Untitled',
+        imgSrc: image.src.medium,
+        apiName: 'Pexels',
+        photographer: image.photographer,
+        portfolioLink: image.photographer_url
+    }  
+  } else if (image.hasOwnProperty('webformatURL')){
+    config = {
+        imageTitle: image.tags ? image.tags : 'Untitled',
+        imgSrc: image.webformatURL,
+        apiName: 'Pixabay',
+        photographer: image.user,
+        portfolioLink: image.userImageURL
+    }   
+  } else {
+      console.log('hasOwnProperty conditional did not work in ImageCardQueueNew'); 
+  }
+
+
+    return (
+        <Card style={{ gridRowEnd: `span ${spans}`, width: '800px'}} className='queueCard'>
+            <div style={{display: 'flex'}}>
+            <Card.Img 
+                className="queueImage" 
+                ref={ref}
+                src={config.imgSrc} 
+                variant="top" 
+            />
+            <Card.Body>
+                <div style={{display: 'flex', justifyContent: 'space-between'}}>
+                    <Card.Title>{config.imageTitle}</Card.Title>
+                    <CloseButton 
+                        onClick={deselect}
+                        className="closeButton"
+                        style={{color: 'white', opacity: '0.5'}}
+                        variant="dark">
+                    </CloseButton>
+                </div>
+                <Card.Text>
+                    <p>Image Source: {config.apiName}</p>
+                    <p>Photographer: {config.photographer}</p>
+                    <a href={config.portfolioLink}>Photographer Portfolio</a>
+                </Card.Text>
+            </Card.Body>
+            </div>
+        </Card>
+    )
 }
 
 export default ImageCardQueue; 
